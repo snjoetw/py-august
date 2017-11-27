8f6b78c5f1ae030a1dd24334d1f3966632322919
Joe Lu <jlu9@apple.com>
2017-11-26 23:12:12 -0800

    Bumper version to 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='py-august',
-    version='0.1.0',
+    version='0.2.0',
     packages=['august'],
     url='https://github.com/snjoetw/py-august',
     license='MIT',
