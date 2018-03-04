0d743d52200e4919e95491027b6969d74bc79767
Joe Lu <jlu9@apple.com>
2018-03-03 20:53:00 -0800

    Bumped up version to 0.4.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='py-august',
-    version='0.3.0',
+    version='0.4.0',
     packages=['august'],
     url='https://github.com/snjoetw/py-august',
     license='MIT',
