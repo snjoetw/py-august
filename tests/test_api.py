1ebd90684a05e1b622f68a4b08e44810be0a6fa2
J. Nick Koston <nick@cpanel.net>
2020-02-10 13:07:41 -0600

    Add tests for get_house_activities

--- tests/test_api.py
+++ tests/test_api.py
@@ -5,10 +5,19 @@ from datetime import datetime
 import requests_mock
 from dateutil.tz import tzutc
 
-from august.api import (API_GET_DOORBELL_URL, API_GET_DOORBELLS_URL,
-                        API_GET_LOCK_STATUS_URL, API_GET_LOCK_URL,
-                        API_GET_LOCKS_URL, API_GET_PINS_URL, API_LOCK_URL,
-                        API_UNLOCK_URL, Api)
+import august.activity
+from august.api import (
+    API_GET_DOORBELL_URL,
+    API_GET_DOORBELLS_URL,
+    API_GET_HOUSE_ACTIVITIES_URL,
+    API_GET_LOCK_STATUS_URL,
+    API_GET_LOCK_URL,
+    API_GET_LOCKS_URL,
+    API_GET_PINS_URL,
+    API_LOCK_URL,
+    API_UNLOCK_URL,
+    Api,
+)
 from august.lock import LockDoorStatus, LockStatus
 
 ACCESS_TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9"
@@ -16,27 +25,24 @@ ACCESS_TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9"
 
 def load_fixture(filename):
     """Load a fixture."""
-    path = os.path.join(os.path.dirname(__file__), 'fixtures', filename)
+    path = os.path.join(os.path.dirname(__file__), "fixtures", filename)
     with open(path) as fptr:
         return fptr.read()
 
 
 def utc_of(year, month, day, hour, minute, second, microsecond):
-    return datetime(year, month, day, hour, minute, second, microsecond,
-                    tzinfo=tzutc())
+    return datetime(year, month, day, hour, minute, second, microsecond, tzinfo=tzutc())
 
 
 class TestApi(unittest.TestCase):
     @requests_mock.Mocker()
     def test_get_doorbells(self, mock):
         mock.register_uri(
-            "get",
-            API_GET_DOORBELLS_URL,
-            text=load_fixture("get_doorbells.json"))
+            "get", API_GET_DOORBELLS_URL, text=load_fixture("get_doorbells.json")
+        )
 
         api = Api()
-        doorbells = sorted(api.get_doorbells(ACCESS_TOKEN),
-                           key=lambda d: d.device_id)
+        doorbells = sorted(api.get_doorbells(ACCESS_TOKEN), key=lambda d: d.device_id)
 
         self.assertEqual(2, len(doorbells))
 
@@ -55,8 +61,7 @@ class TestApi(unittest.TestCase):
         self.assertEqual("Front Door", second.device_name)
         self.assertEqual("doorbell_call_status_online", second.status)
         self.assertEqual(True, second.has_subscription)
-        self.assertEqual("https://image.com/vmk16naaaa7ibuey7sar.jpg",
-                         second.image_url)
+        self.assertEqual("https://image.com/vmk16naaaa7ibuey7sar.jpg", second.image_url)
         self.assertEqual("3dd2accaea08", second.house_id)
 
     @requests_mock.Mocker()
@@ -64,7 +69,8 @@ class TestApi(unittest.TestCase):
         mock.register_uri(
             "get",
             API_GET_DOORBELL_URL.format(doorbell_id="K98GiDT45GUL"),
-            text=load_fixture("get_doorbell.json"))
+            text=load_fixture("get_doorbell.json"),
+        )
 
         api = Api()
         doorbell = api.get_doorbell_detail(ACCESS_TOKEN, "K98GiDT45GUL")
@@ -77,15 +83,13 @@ class TestApi(unittest.TestCase):
         self.assertEqual("doorbell_call_status_online", doorbell.status)
         self.assertEqual(True, doorbell.is_online)
         self.assertEqual(True, doorbell.has_subscription)
-        self.assertEqual("https://image.com/vmk16naaaa7ibuey7sar.jpg",
-                         doorbell.image_url)
+        self.assertEqual(
+            "https://image.com/vmk16naaaa7ibuey7sar.jpg", doorbell.image_url
+        )
 
     @requests_mock.Mocker()
     def test_get_locks(self, mock):
-        mock.register_uri(
-            "get",
-            API_GET_LOCKS_URL,
-            text=load_fixture("get_locks.json"))
+        mock.register_uri("get", API_GET_LOCKS_URL, text=load_fixture("get_locks.json"))
 
         api = Api()
         locks = sorted(api.get_locks(ACCESS_TOKEN), key=lambda d: d.device_id)
@@ -106,10 +110,7 @@ class TestApi(unittest.TestCase):
 
     @requests_mock.Mocker()
     def test_get_operable_locks(self, mock):
-        mock.register_uri(
-            "get",
-            API_GET_LOCKS_URL,
-            text=load_fixture("get_locks.json"))
+        mock.register_uri("get", API_GET_LOCKS_URL, text=load_fixture("get_locks.json"))
 
         api = Api()
         locks = api.get_operable_locks(ACCESS_TOKEN)
@@ -126,13 +127,12 @@ class TestApi(unittest.TestCase):
     def test_get_lock_detail(self, mock):
         mock.register_uri(
             "get",
-            API_GET_LOCK_URL.format(
-                lock_id="A6697750D607098BAE8D6BAA11EF8063"),
-            text=load_fixture("get_lock.json"))
+            API_GET_LOCK_URL.format(lock_id="A6697750D607098BAE8D6BAA11EF8063"),
+            text=load_fixture("get_lock.json"),
+        )
 
         api = Api()
-        lock = api.get_lock_detail(ACCESS_TOKEN,
-                                   "A6697750D607098BAE8D6BAA11EF8063")
+        lock = api.get_lock_detail(ACCESS_TOKEN, "A6697750D607098BAE8D6BAA11EF8063")
 
         self.assertEqual("A6697750D607098BAE8D6BAA11EF8063", lock.device_id)
         self.assertEqual("Front Door Lock", lock.device_name)
@@ -149,7 +149,8 @@ class TestApi(unittest.TestCase):
         mock.register_uri(
             "get",
             API_GET_LOCK_STATUS_URL.format(lock_id=lock_id),
-            text="{\"status\": \"kAugLockState_Locked\"}")
+            text='{"status": "kAugLockState_Locked"}',
+        )
 
         api = Api()
         status = api.get_lock_status(ACCESS_TOKEN, lock_id)
@@ -162,8 +163,9 @@ class TestApi(unittest.TestCase):
         mock.register_uri(
             "get",
             API_GET_LOCK_STATUS_URL.format(lock_id=lock_id),
-            text="{\"status\": \"kAugLockState_Locked\""
-                 ",\"doorState\": \"kAugLockDoorState_Closed\"}")
+            text='{"status": "kAugLockState_Locked"'
+            ',"doorState": "kAugLockDoorState_Closed"}',
+        )
 
         api = Api()
         status, door_status = api.get_lock_status(ACCESS_TOKEN, lock_id, True)
@@ -177,7 +179,8 @@ class TestApi(unittest.TestCase):
         mock.register_uri(
             "get",
             API_GET_LOCK_STATUS_URL.format(lock_id=lock_id),
-            text="{\"status\": \"kAugLockState_Unlocked\"}")
+            text='{"status": "kAugLockState_Unlocked"}',
+        )
 
         api = Api()
         status = api.get_lock_status(ACCESS_TOKEN, lock_id)
@@ -190,7 +193,8 @@ class TestApi(unittest.TestCase):
         mock.register_uri(
             "get",
             API_GET_LOCK_STATUS_URL.format(lock_id=lock_id),
-            text="{\"status\": \"not_advertising\"}")
+            text='{"status": "not_advertising"}',
+        )
 
         api = Api()
         status = api.get_lock_status(ACCESS_TOKEN, lock_id)
@@ -203,7 +207,8 @@ class TestApi(unittest.TestCase):
         mock.register_uri(
             "get",
             API_GET_LOCK_STATUS_URL.format(lock_id=lock_id),
-            text="{\"doorState\": \"kAugLockDoorState_Closed\"}")
+            text='{"doorState": "kAugLockDoorState_Closed"}',
+        )
 
         api = Api()
         door_status = api.get_lock_door_status(ACCESS_TOKEN, lock_id)
@@ -216,7 +221,8 @@ class TestApi(unittest.TestCase):
         mock.register_uri(
             "get",
             API_GET_LOCK_STATUS_URL.format(lock_id=lock_id),
-            text="{\"doorState\": \"kAugLockDoorState_Open\"}")
+            text='{"doorState": "kAugLockDoorState_Open"}',
+        )
 
         api = Api()
         door_status = api.get_lock_door_status(ACCESS_TOKEN, lock_id)
@@ -229,12 +235,12 @@ class TestApi(unittest.TestCase):
         mock.register_uri(
             "get",
             API_GET_LOCK_STATUS_URL.format(lock_id=lock_id),
-            text="{\"status\": \"kAugLockState_Unlocked\""
-                 ",\"doorState\": \"kAugLockDoorState_Open\"}")
+            text='{"status": "kAugLockState_Unlocked"'
+            ',"doorState": "kAugLockDoorState_Open"}',
+        )
 
         api = Api()
-        door_status, status = api.get_lock_door_status(ACCESS_TOKEN, lock_id,
-                                                       True)
+        door_status, status = api.get_lock_door_status(ACCESS_TOKEN, lock_id, True)
 
         self.assertEqual(LockDoorStatus.OPEN, door_status)
         self.assertEqual(LockStatus.UNLOCKED, status)
@@ -245,7 +251,8 @@ class TestApi(unittest.TestCase):
         mock.register_uri(
             "get",
             API_GET_LOCK_STATUS_URL.format(lock_id=lock_id),
-            text="{\"doorState\": \"not_advertising\"}")
+            text='{"doorState": "not_advertising"}',
+        )
 
         api = Api()
         door_status = api.get_lock_door_status(ACCESS_TOKEN, lock_id)
@@ -258,10 +265,11 @@ class TestApi(unittest.TestCase):
         mock.register_uri(
             "put",
             API_LOCK_URL.format(lock_id=lock_id),
-            text="{\"status\":\"locked\","
-                 "\"dateTime\":\"2017-12-10T07:43:39.056Z\","
-                 "\"isLockStatusChanged\":false,"
-                 "\"valid\":true}")
+            text='{"status":"locked",'
+            '"dateTime":"2017-12-10T07:43:39.056Z",'
+            '"isLockStatusChanged":false,'
+            '"valid":true}',
+        )
 
         api = Api()
         status = api.lock(ACCESS_TOKEN, lock_id)
@@ -272,9 +280,8 @@ class TestApi(unittest.TestCase):
     def test_unlock(self, mock):
         lock_id = 1234
         mock.register_uri(
-            "put",
-            API_UNLOCK_URL.format(lock_id=lock_id),
-            text="{\"status\": \"unlocked\"}")
+            "put", API_UNLOCK_URL.format(lock_id=lock_id), text='{"status": "unlocked"}'
+        )
 
         api = Api()
         status = api.unlock(ACCESS_TOKEN, lock_id)
@@ -287,7 +294,8 @@ class TestApi(unittest.TestCase):
         mock.register_uri(
             "get",
             API_GET_PINS_URL.format(lock_id=lock_id),
-            text=load_fixture("get_pins.json"))
+            text=load_fixture("get_pins.json"),
+        )
 
         api = Api()
         pins = api.get_pins(ACCESS_TOKEN, lock_id)
@@ -305,15 +313,34 @@ class TestApi(unittest.TestCase):
         self.assertEqual("John", first.first_name)
         self.assertEqual("Doe", first.last_name)
         self.assertEqual(True, first.unverified)
-        self.assertEqual(utc_of(2016, 11, 26, 22, 27, 11, 176000),
-                         first.created_at)
-        self.assertEqual(utc_of(2017, 11, 23, 00, 42, 19, 470000),
-                         first.updated_at)
-        self.assertEqual(utc_of(2017, 12, 10, 3, 12, 55, 563000),
-                         first.loaded_date)
-        self.assertEqual(utc_of(2018, 1, 1, 1, 1, 1, 563000),
-                         first.access_start_time)
-        self.assertEqual(utc_of(2018, 12, 1, 1, 1, 1, 563000),
-                         first.access_end_time)
-        self.assertEqual(utc_of(2018, 11, 5, 10, 2, 41, 684000),
-                         first.access_times)
+        self.assertEqual(utc_of(2016, 11, 26, 22, 27, 11, 176000), first.created_at)
+        self.assertEqual(utc_of(2017, 11, 23, 00, 42, 19, 470000), first.updated_at)
+        self.assertEqual(utc_of(2017, 12, 10, 3, 12, 55, 563000), first.loaded_date)
+        self.assertEqual(utc_of(2018, 1, 1, 1, 1, 1, 563000), first.access_start_time)
+        self.assertEqual(utc_of(2018, 12, 1, 1, 1, 1, 563000), first.access_end_time)
+        self.assertEqual(utc_of(2018, 11, 5, 10, 2, 41, 684000), first.access_times)
+
+    @requests_mock.Mocker()
+    def test_get_house_activities(self, mock):
+        house_id = 1234
+        mock.register_uri(
+            "get",
+            API_GET_HOUSE_ACTIVITIES_URL.format(house_id=house_id),
+            text=load_fixture("get_house_activities.json"),
+        )
+
+        api = Api()
+        activities = api.get_house_activities(ACCESS_TOKEN, house_id)
+
+        self.assertEqual(10, len(activities))
+
+        self.assertIsInstance(activities[0], august.activity.LockOperationActivity)
+        self.assertIsInstance(activities[1], august.activity.LockOperationActivity)
+        self.assertIsInstance(activities[2], august.activity.LockOperationActivity)
+        self.assertIsInstance(activities[3], august.activity.LockOperationActivity)
+        self.assertIsInstance(activities[4], august.activity.LockOperationActivity)
+        self.assertIsInstance(activities[5], august.activity.DoorOperationActivity)
+        self.assertIsInstance(activities[6], august.activity.DoorOperationActivity)
+        self.assertIsInstance(activities[7], august.activity.DoorOperationActivity)
+        self.assertIsInstance(activities[8], august.activity.LockOperationActivity)
+        self.assertIsInstance(activities[9], august.activity.LockOperationActivity)
