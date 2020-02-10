3e2a646c9df9c9f092536715bf56e90b1391cce1
Joe Lu <snjoetw@gmail.com>
2020-02-10 07:14:43 -0800

    - bumped version to 0.11.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='py-august',
-    version='0.10.1',
+    version='0.11.0',
     packages=['august'],
     url='https://github.com/snjoetw/py-august',
     license='MIT',
