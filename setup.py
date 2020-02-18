6beafb0e1a22de1242ad9448ad1b94dbf64a55de
J. Nick Koston <nick@cpanel.net>
2020-02-17 20:08:01 -0600

    Add door status to lock detail
    In order to reduce the number of api calls we can
    now get the door status from the single lock detail
    endpoint since it is contained inside the detail api.
    
    Provide sets for the lock status and door state in
    the LockDetail so they can be updated with data
    from the activity api

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='py-august',
-    version='0.14.0',
+    version='0.15.0',
     packages=['august'],
     url='https://github.com/snjoetw/py-august',
     license='MIT',
