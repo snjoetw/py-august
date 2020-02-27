e6eaa3ed3ee5d5e49369f8d0bb63f0e1a4d316a5
J. Nick Koston <nick@koston.org>
2020-02-27 00:17:15 -1000

    Additional battery percentage estimations
    * Battery percentage for the mars2 lock
    
    * Battery percentage for the keypad
    
    These mirror the name to percentage mappings
    currently used in home assistant

--- august/keypad.py
+++ august/keypad.py
@@ -1,5 +1,9 @@
 from august.device import DeviceDetail
 
+BATTERY_LEVEL_FULL = "Full"
+BATTERY_LEVEL_MEDIUM = "Medium"
+BATTERY_LEVEL_LOW = "Low"
+
 
 class KeypadDetail(DeviceDetail):
     def __init__(self, house_id, keypad_name, data):
@@ -8,7 +12,7 @@ class KeypadDetail(DeviceDetail):
             keypad_name,
             house_id,
             data["serialNumber"],
-            data["currentFirmwareVersion"]
+            data["currentFirmwareVersion"],
         )
 
         self._battery_level = data["batteryLevel"]
@@ -20,3 +24,18 @@ class KeypadDetail(DeviceDetail):
     @property
     def battery_level(self):
         return self._battery_level
+
+    @property
+    def battery_percentage(self):
+        """Return an approximation of the battery percentage."""
+        if not self._battery_level:
+            return None
+
+        if self._battery_level == BATTERY_LEVEL_FULL:
+            return 100
+        if self._battery_level == BATTERY_LEVEL_MEDIUM:
+            return 60
+        if self._battery_level == BATTERY_LEVEL_LOW:
+            return 10
+
+        return 0
